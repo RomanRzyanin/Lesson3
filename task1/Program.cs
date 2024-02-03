3b897a75375ddfea0869b6e4b3d146affc7011f2
Roman Rzyanin <roman@rzyanin.ru>
2024-02-03 15:02:38 +0300

    Lesson3:task1,2,3

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,21 +1,23 @@
-﻿// int n = 11;
-// int i = 1;
+﻿// task1
 
-// while (i <= n)
+// double calculateFormula(int a, int b, int c, int d)
 // {
-//     Console.Write($"{i * i} ");
-//     i++;
+//     double numerator = a * b;
+//     int denomenator = c + d;
+//     double result = numerator / denomenator;
+//     return result;
 // }
+// //calculateFormula(1, 2, 3, 4);
+// double result = calculateFormula(1, 2, 3, 4);
+// Console.WriteLine(result);
 
-// task1
+//task2
 
-double calculateFormula(int a, int b, int c, int d)
+int n = 11;
+int i = 1;
+
+while (i <= n)
 {
-    double numerator = a * b;
-    int denomenator = c + d;
-    double result = numerator / denomenator;
-    return result;
+    Console.Write($"{i * i} ");
+    i++;
 }
-//calculateFormula(1, 2, 3, 4);
-double result = calculateFormula(1, 2, 3, 4);
-Console.WriteLine(result);
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//task2
+
+void printSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+ }
+ printSquares (5);
+ printSquares (10);
+ printSquares (15); 
