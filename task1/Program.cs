d816c8c4ba26d0e0fcdfb8b62b0e374a99adf503
Roman Rzyanin <roman@rzyanin.ru>
2024-02-03 13:31:07 +0300

    Lesson3:task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// int n = 11;
+// int i = 1;
+
+// while (i <= n)
+// {
+//     Console.Write($"{i * i} ");
+//     i++;
+// }
+
+// task1
+
+double calculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denomenator = c + d;
+    double result = numerator / denomenator;
+    return result;
+}
+//calculateFormula(1, 2, 3, 4);
+double result = calculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
